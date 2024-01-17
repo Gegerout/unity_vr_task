c4f74fba5a23f3a36a7861a6d5de653de8422dca
Gegerout <selimchik06@gmail.com>
2024-01-17 22:32:30 +0300

    weather controller update

--- Assets/Scripts/Environment/WeatherController.cs
+++ Assets/Scripts/Environment/WeatherController.cs
@@ -1,26 +1,36 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.VFX;
 
 public class WeatherController : MonoBehaviour
 {
     public enum WeatherType
     {
         GoodWeather,
-        BadWeather
-        /*
-        Fog,
-        Storm,
-        ModerateWaves,
-        StrongWaves*/
+        GoodWeatherWithLightWind,
+        BadWeatherRainy,
+        BadWeatherFoggy,
+        BadWeatherRainyAndFoggy
     }
 
+
+    [Header("Main Settings"), SerializeField]
+    private float weatherChangeTime = 60f;
     [Header("Fog Settings"), SerializeField]
-    private float fogDensity = 0.2f;
-    [SerializeField]
-    private float fogChangeSpeed = 0.5f;
+    private Volume volume;
+    [SerializeField, Range(0, 1f)]
+    private float fogIntensity;
+    [Header("Rain Settings"), SerializeField]
+    private VisualEffect rainVFX;
+    [SerializeField, Range(0, 1f)]
+    private float rainIntensity;
 
     private float lastWeatherChangeTime;
-
+    private float fogFadeTime = 10f;
+    private float rainFadeTime = 1f;
     public static event Action<WeatherType> OnWeatherChanged;
 
     private void Start()
@@ -31,7 +41,7 @@ public class WeatherController : MonoBehaviour
 
     private void Update()
     {
-        if (Time.time - lastWeatherChangeTime >= 5f)
+        if (Time.time - lastWeatherChangeTime >= weatherChangeTime)
         {
             ChangeWeather();
             lastWeatherChangeTime = Time.time;
@@ -45,42 +55,97 @@ public class WeatherController : MonoBehaviour
         switch (newWeather)
         {
             case WeatherType.GoodWeather:
+                SetGoodWeather();
                 break;
-            case WeatherType.BadWeather:
+            case WeatherType.GoodWeatherWithLightWind:
+                SetGoodWeatherWithLightWind();
+                break;
+            case WeatherType.BadWeatherRainy:
+                SetBadWeatherRainy();
+                break;
+            case WeatherType.BadWeatherFoggy:
+                SetBadWeatherFoggy();
+                break;
+            case WeatherType.BadWeatherRainyAndFoggy:
+                SetBadWeatherRainyAndFoggy();
                 break;
         }
 
         NotifyWeatherChanged(newWeather);
     }
 
-    /*private void SetGoodWeather()
+    private void SetGoodWeather()
     {
-        StartCoroutine(ChangeFog(fogDensity, fogChangeSpeed, false));
+        StartCoroutine(ChangeFog(false));
+        StartCoroutine(ChangeRain(false));
     }
 
-    private IEnumerator ChangeFog(float targetDensity, float speed, bool add)
+    private void SetGoodWeatherWithLightWind()
     {
-        if(add)
+        StartCoroutine(ChangeFog(false));
+        StartCoroutine(ChangeRain(false));
+    }
+
+    private void SetBadWeatherRainy()
+    {
+        StartCoroutine(ChangeFog(false));
+        StartCoroutine(ChangeRain(true));
+    }
+
+    private void SetBadWeatherFoggy()
+    {
+        StartCoroutine(ChangeFog(true));
+        StartCoroutine(ChangeRain(false));
+    }
+
+    private void SetBadWeatherRainyAndFoggy()
+    {
+        StartCoroutine(ChangeFog(true));
+        StartCoroutine(ChangeRain(true));
+    }
+
+    private IEnumerator ChangeFog(bool add)
+    {
+        VolumeProfile profile = volume.sharedProfile;
+
+        if (!profile.TryGet<Fog>(out var fog))
         {
-            RenderSettings.fog = true;
-            while (RenderSettings.fogDensity < targetDensity)
-            {
-                RenderSettings.fogDensity += speed * Time.deltaTime;
-                yield return null;
-            }
-        } else
-        {
-            while (RenderSettings.fogDensity > 0f)
-            {
-                RenderSettings.fogDensity -= speed * Time.deltaTime;
-                if(RenderSettings.fogDensity <= 0f)
-                {
-                    RenderSettings.fog = false;
-                }
-                yield return null;
-            }
+            fog = profile.Add<Fog>(false);
         }
-    }*/
+
+        float startValue = fog.meanFreePath.value;
+        float endValue = add ? fogIntensity * 10 : 1000f;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fogFadeTime)
+        {
+            float t = Mathf.SmoothStep(0f, 1f, elapsedTime / fogFadeTime);
+            elapsedTime += Time.deltaTime;
+
+            fog.meanFreePath.value = Mathf.Lerp(startValue, endValue, t);
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ChangeRain(bool add)
+    {
+        float startValue = rainVFX.GetFloat("Intensity");
+        float endValue = add ? rainIntensity : 0f;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < rainFadeTime)
+        {
+            float t = Mathf.SmoothStep(0f, 1f, elapsedTime / rainFadeTime);
+            elapsedTime += Time.deltaTime;
+
+            rainVFX.SetFloat("Intensity", Mathf.Lerp(startValue, endValue, t));
+
+            yield return null;
+        }
+    }
 
     private void NotifyWeatherChanged(WeatherType newWeather)
     {
